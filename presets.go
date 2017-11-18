e172839af832d8fc791b3e54757f6df42c790095
Lauri Piispanen <lauri.piispanen@reaktor.com>
2017-11-18 13:06:20 +0200

    fix denmark

--- presets.go
+++ presets.go
@@ -5,14 +5,14 @@ type PresetLocations []string
 var PRESETS = map[string]PresetLocations{
   "finland":PresetLocations{"finland", "suomi", "helsinki", "tampere", "oulu", "espoo", "vantaa", "turku"},
   "sweden":PresetLocations{"sweden", "sverige", "stockholm", "malm%C3%B6", "uppsala", "g%C3%B6teborg", "gothenburg"},
-  "denmark":PresetLocations{"denmark", "danmark", "copenhagen", "københavn"},
   "norway":PresetLocations{"norway", "norge", "oslo", "bergen"},
   "germany":PresetLocations{"germany", "deutschland", "berlin", "frankfurt", "munich", "m%C3%BCnchen", "hamburg", "cologne", "k%C3%B6ln"},
   "netherlands":PresetLocations{"netherlands", "nederland", "amsterdam", "rotterdam", "hague", "utrecht", "holland"},
   "ukraine":PresetLocations{"ukraine", "kiev", "kharkiv", "dnipro", "odesa", "donetsk", "zaporizhia"},
   "japan":PresetLocations{"japan", "tokyo", "yokohama", "osaka", "nagoya", "sapporo", "kobe", "kyoto", "fukuoka", "kawasaki", "saitama", "hiroshima", "sendai"},
   "russia":PresetLocations{"russia", "moscow", "saint%2Bpetersburg", "novosibirsk", "yekaterinburg", "nizhny%2Bnovgorod", "samara", "omsk", "kazan", "chelyabinsk", "rostov-on-don", "ufa", "volgograd"},
-  "estonia":PresetLocations{"estonia", "eesti", "tallinn", "tartu", "narva", "p%C3%A4rnu"}}
+  "estonia":PresetLocations{"estonia", "eesti", "tallinn", "tartu", "narva", "p%C3%A4rnu"},
+  "denmark":PresetLocations{"denmark", "danmark", "copenhagen","aarhus","odense","aalborg"}}
 
 func Preset(name string) []string {
   return PRESETS[name]
