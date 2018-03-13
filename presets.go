bf7ee611411d3fff8d99070b52070afa651c6951
Eran Chetz <eran.chetzroni@algolia.com>
2018-03-13 15:29:14 +0100

    Add Israel

--- presets.go
+++ presets.go
@@ -23,6 +23,7 @@ var PRESETS = map[string]PresetLocations{
   "worldwide": PresetLocations{},
   "china": PresetLocations{"china", "%E4%B8%AD%E5%9B%BD", "guangzhou", "shanghai", "beijing", "hangzhou", "hong%2Bkong"},
   "india": PresetLocations{"india", "mumbai", "delhi", "bangalore", "hyderabad", "ahmedabad", "chennai", "kolkata", "jaipur"},
+  "israel": PresetLocations{"israel", "tel-aviv", "tel aviv", "jerusalem", "tel-aviv yafo", "beer sheva", "beersheva","netanya", "ramat-gan", "ramat gan", "haifa", "herzliya"},
   "indonesia": PresetLocations{"indonesia", "jakarta", "surabaya", "bandung", "medan", "bekasi", "semarang", "tangerang", "depok", "makassar", "palembang"},
   "pakistan": PresetLocations{"pakistan", "karachi", "lahore", "faisalabad", "rawalpindi", "peshawar", "islamabad"},
   "brazil": PresetLocations{"brazil", "brasil", "s%C3%A3o%20paulo", "bras%C3%ADlia", "salvador", "fortaleza", "belo%20horizonte", "manaus", "curitiba", "recife", "porto%20alegre"},
