20290116412785674b4f8fd036e18d73013c8ac3
Houari Zegai <houarizegai14@gmail.com>
2019-07-01 00:21:19 +0100

    Add a few cities to Algeria

--- presets.go
+++ presets.go
@@ -46,7 +46,7 @@ var PRESETS = map[string]PresetLocations{
 	"colombia":     PresetLocations{"colombia", "bogota", "medellin", "cali", "barranquilla", "cartagena", "cucuta", "bucaramanga", "ibague", "soledad", "pereira", "santa%2Bmarta"},
 	"kenya":        PresetLocations{"kenya", "nairobi", "mombasa", "kisumu", "nakuru", "eldoret", "kisii"},
 	"argentina":    PresetLocations{"argentina", "buenos%2Baires", "cordoba", "rosario", "mendoza", "la%2Bplata", "tucuman", "mar%2Bdel%2Bplata", "salta", "resistencia"},
-	"algeria":      PresetLocations{"algeria", "algiers", "oran", "constantine", "annaba", "blida", "batna", "djelfa", "setif", "sidi%2Bbel%2Babbes", "biskra"},
+	"algeria":      PresetLocations{"algeria", "algiers", "oran", "constantine", "annaba", "blida", "batna", "djelfa", "setif", "sidi%2Bbel%2Babbes", "biskra", "tiaret", "relizane", "mostaganem", "tlemcen", "chlef", "jijel"},
 	"sudan":        PresetLocations{"sudan", "khartoum", "omdurman"},
 	"poland":       PresetLocations{"poland", "polska", "warsaw", "krakow", "lodz", "wroclaw", "poznan", "gdansk", "szczecin", "bydgoszcz", "lublin", "katowice", "bialystok"},
 	"canada":       PresetLocations{"canada", "ottawa", "edmonton", "winnipeg", "vancouver", "toronto", "quebec", "montreal", "mississauga", "calgary"},
